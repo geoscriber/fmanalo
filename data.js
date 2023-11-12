2b6b1ba182a9ea7ab535dd1ef923a10469419148
geoscriber <100984391+geoscriber@users.noreply.github.com>
2023-11-12 23:30:56 +0800

    init

---
+++ data.js
@@ -0,0 +1,199 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-image-1",
+      "name": "Image 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 3.076593108359626,
+        "pitch": 1.5707963267948966,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0316725305685601,
+          "pitch": 1.309712142532529,
+          "rotation": 0,
+          "target": "2-image-3"
+        },
+        {
+          "yaw": 0.12514361204910784,
+          "pitch": 1.3047350433030225,
+          "rotation": 0,
+          "target": "1-image-2"
+        },
+        {
+          "yaw": 3.031720106114644,
+          "pitch": 1.118184763737334,
+          "rotation": 0,
+          "target": "3-image-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-image-2",
+      "name": "Image 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.35048921818104795,
+        "pitch": 0.5208738882054806,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0321232149865338,
+          "pitch": -0.13425094848081187,
+          "rotation": 0,
+          "target": "2-image-3"
+        },
+        {
+          "yaw": 3.063422838102742,
+          "pitch": -0.25878704227276117,
+          "rotation": 0,
+          "target": "3-image-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-image-3",
+      "name": "Image 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.4987152520521754,
+        "pitch": 0.9355632348065797,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4237884560975083,
+          "pitch": 1.5303881569995506,
+          "rotation": 0,
+          "target": "1-image-2"
+        },
+        {
+          "yaw": 3.1175410614222523,
+          "pitch": 0.29277627856607147,
+          "rotation": 0,
+          "target": "3-image-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-image-4",
+      "name": "Image 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.19669583633584864,
+        "pitch": 0.8387051068251292,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05118068834743639,
+          "pitch": 0.5232864227625402,
+          "rotation": 0,
+          "target": "1-image-2"
+        },
+        {
+          "yaw": -0.1275665863493174,
+          "pitch": 0.2657179765275295,
+          "rotation": 0,
+          "target": "2-image-3"
+        },
+        {
+          "yaw": 0.21528776049925824,
+          "pitch": -0.05828185034259903,
+          "rotation": 0,
+          "target": "0-image-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "F. Manalo (Flood Gate)",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
